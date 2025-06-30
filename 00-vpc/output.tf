46d729fcffb34232c269b37104127218e89d0fbe
Ramesh Katikam <katikam.rams@gmail.com>
2025-06-30 22:28:41 +0530

    terraform vpc code changes

---
+++ 00-vpc/output.tf
@@ -0,0 +1,8 @@
+/* output "azs_info" {
+  value = module.vpc.azs_info
+} */
+
+/* output "public_subnet_info" {
+    value = module.vpc.public_subnet_info
+} */
+
---
+++ 00-vpc/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.99.1"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "roboshop-ramesh-remote-state-dev"
+    key          = "roboshop-vpc-dev"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
