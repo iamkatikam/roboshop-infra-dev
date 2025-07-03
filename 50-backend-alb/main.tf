1810ad878fd99dc189dd29fd023391704756a040
Ramesh Katikam <katikam.rams@gmail.com>
2025-07-03 13:19:20 +0530

    backend ALB code changes

---
+++ 50-backend-alb/main.tf
@@ -0,0 +1,35 @@
+module "backend_alb" {
+  source = "terraform-aws-modules/alb/aws"
+    internal = true
+  name    = "${var.project}-${var.environment}-backend-alb"
+  vpc_id  = local.vpc_id
+  subnets = local.private_subnet_ids
+  create_security_group = false
+    security_groups = [local.backend_alb_sg_id]
+    #enable_deletion_protection = true
+
+ 
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-backend-alb"
+    }
+  )
+}
+
+
+resource "aws_lb_listener" "backend_alb" {
+  load_balancer_arn = module.backend_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "I am from backend ALB"
+      status_code  = "200"
+    }
+  }
+}
