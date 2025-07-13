ae32e4087cc3d80c344a00275f418da89dc53633
Ramesh Katikam <katikam.rams@gmail.com>
2025-07-13 11:40:03 +0530

    increase memory for bastion

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,14 @@ resource "aws_instance" "bastion" {
   instance_type = "t2.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_id
+    # need more for terraform
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3" # or "gp2", depending on your preference
+  }
+  user_data = file("bastion.sh")
+  iam_instance_profile = "RoleForVPNHost"
+
   tags = merge(
     local.common_tags,
     {
